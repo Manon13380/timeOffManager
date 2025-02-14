cbd22ff19dd3d7b22ab46f7147c72ff9e77d156a
manon13380 <manon.carpentieri@hotmail.com>
2025-02-14 10:21:44 +0100

    add register

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,12 +7,14 @@ use Doctrine\Common\Collections\Collection;
 use Stringable;
 use Doctrine\ORM\Mapping as ORM;
 use App\Repository\UserRepository;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface, \Stringable
 {
     #[ORM\Id]
